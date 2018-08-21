6841a17b3664d278e28f356152f17957a5eb6641
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-21 05:54:37 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,9 +4,9 @@
 def display_board(board)
   i = 0
   2.times do
-    puts " #{board[i * 3]} | #{board[i * 3 + 1]} | #{board[i * 3 + 2]}"
+    puts " #{board[i * 3]} | #{board[i * 3 + 1]} | #{board[i * 3 + 2]} "
     puts "----------"
     i = i + 1
   end
-  puts " #{board[6]} | #{board[7]} | #{board[8]}"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
